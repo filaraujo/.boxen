420901d6a14ea1d5f92de691c86d7b9dd69d5930
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-01-16 20:31:38 -0500

    fixing sublimelinter

--- modules/people/manifests/filaraujo/applications.pp
+++ modules/people/manifests/filaraujo/applications.pp
@@ -25,15 +25,13 @@ class people::filaraujo::applications inherits people::filaraujo {
 	sublime_text_3::package { 'Theme - Centurion': source => 'allanhortle/Centurion' }
 	sublime_text_3::package { 'Color Scheme - Brackets': source => 'l4ci/Br4ckets-Theme' }
 
-
-    sublime_text_3::package { 'Sublime-JSLint': source => 'darrenderidder/Sublime-JSLint' }
 	sublime_text_3::package { 'PreferenceHelper': source => 'Chris---/SublimeText-Preference-Helper' }
 	sublime_text_3::package { 'SublimeGit': source => 'kemayo/sublime-text-git' }
 	sublime_text_3::package { 'CSSComb': source => 'csscomb/csscomb-for-sublime' }
 	sublime_text_3::package { 'Emmet': source => 'sergeche/emmet-sublime' }
 	sublime_text_3::package { 'SASS': source => 'nathos/sass-textmate-bundle' }
 	sublime_text_3::package { 'Swig': source => 'jonschlinkert/sublime-swig' }
-	sublime_text_3::package { 'SublimeLinter': source => 'https://github.com/SublimeLinter/SublimeLinter' }
+	sublime_text_3::package { 'SublimeLinter': source => 'SublimeLinter/SublimeLinter' }
 
     # dashlane
   	include dashlane
