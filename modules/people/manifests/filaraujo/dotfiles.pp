9a37e0a67f829c78d9fb1b92b66123b9b0c9d88a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-01-28 16:55:45 -0500

    adding functions files

--- modules/people/manifests/filaraujo/dotfiles.pp
+++ modules/people/manifests/filaraujo/dotfiles.pp
@@ -1,7 +1,7 @@
 class people::filaraujo::dotfiles inherits people::filaraujo {
-    
+
     notify { "loading dotfiles": }
-    
+
     # define dotfile repo
     repository { "${boxen::config::srcdir}/dotfiles":
         source  => 'filaraujo/dotfiles'
@@ -25,10 +25,16 @@ class people::filaraujo::dotfiles inherits people::filaraujo {
         require  => Repository["${boxen::config::srcdir}/dotfiles"]
     }
 
+    file { "functions":
+        path    => "/Users/${::luser}/.functions",
+        source  => "${boxen::config::srcdir}/dotfiles/.functions",
+        require  => Repository["${boxen::config::srcdir}/dotfiles"]
+    }
+
     file { "gitconfig":
         path    => "/Users/${::luser}/.gitconfig",
         source  => "${boxen::config::srcdir}/dotfiles/.gitconfig",
         require  => Repository["${boxen::config::srcdir}/dotfiles"]
     }
-    
+
 }
